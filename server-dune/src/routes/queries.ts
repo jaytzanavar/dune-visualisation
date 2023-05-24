91a5a2b63796c3724310ef5c6183433699f6df85
George <georges@real-estate-executive.ch>
2023-05-24 18:02:18 +0200

    Server: adds try catch sections

--- server-dune/src/routes/queries.ts
+++ server-dune/src/routes/queries.ts
@@ -15,14 +15,24 @@ queryRoutes.get("/:id", async (req, res) => {
   console.log(" querying dune for query... ", req.params.id);
   const { id } = req.params;
   const { API_KEY } = process.env;
-  let client: DuneClient;
-  if (process.env.API_KEY && id) {
-    client = new DuneClient(API_KEY ?? "");
-    client.refresh(Number(id)).then((executionResult) => {
-      console.log(executionResult.result?.rows);
-      res.status(200).send(executionResult);
-    });
+  try {
+    let client: DuneClient;
+    if (process.env.API_KEY && id) {
+      client = new DuneClient(API_KEY ?? "");
 
-    //const queryID = ;
+      client
+        .refresh(Number(id))
+        .then((executionResult) => {
+          console.log(executionResult.result?.rows);
+          res.status(200).send(executionResult);
+        })
+        .catch((err) => {
+          console.log("ERROR in query", err);
+        });
+
+      //const queryID = ;
+    }
+  } catch (error) {
+    res.status(500).json({ message: error });
   }
 });
